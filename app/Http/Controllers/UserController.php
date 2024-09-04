a12dce2af8fe821ce1d19cc1275e4c9d32aaeac9
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2024-09-04 15:17:21 +0800

    using single token

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ServerToken;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
 
@@ -10,28 +11,51 @@ class UserController extends Controller
     function index()
     {
 
-        $tokenRequestClient = new Client;
+        do {
 
-        $response = $tokenRequestClient->post('http://127.0.0.1:8000/oauth/token', [
-            'form_params' => [
-                'grant_type' => 'client_credentials',
-                'client_id' => '4',
-                'client_secret' => '8Hul1rqrPc6zhQ2ZaKloF7T7HG65kEQudYXjPHxl',
-            ],
-        ]);
+            $serverToken = ServerToken::where('status', 'active')->first();
+            if (!$serverToken) {
+                $tokenRequestClient = new Client;
 
-        $token = json_decode((string) $response->getBody(), true);
-        dd($token);
-        $client = new Client([
-            'verify' => false,
-        ]);
+                $response = $tokenRequestClient->post('http://127.0.0.1:8000/oauth/token', [
+                    'form_params' => [
+                        'grant_type' => 'client_credentials',
+                        'client_id' => '4',
+                        'client_secret' => '8Hul1rqrPc6zhQ2ZaKloF7T7HG65kEQudYXjPHxl',
+                    ],
+                ]);
+                $token = json_decode((string) $response->getBody(), true);
+                $serverToken = new ServerToken;
+                $serverToken->token = $token['access_token'];
+                $serverToken->save();
+            }
 
-        $response = $client->Request('GET', 'http://127.0.0.1:8000/api/getallusers', [
-            'headers' => [
-                'Accept' => 'application/json',
-                'Authorization' => 'Bearer ' . $token['access_token']
-            ]
-        ]);
+            try {
+                $token = $serverToken->token;
+
+                $client = new Client([
+                    'verify' => false,
+                ]);
+
+                $response = $client->Request('GET', 'http://127.0.0.1:8000/api/getallusers', [
+                    'headers' => [
+                        'Accept' => 'application/json',
+                        'Authorization' => 'Bearer ' . $token
+                    ]
+                ]);
+
+                $success = true;
+
+            } catch (\Throwable $th) {
+                if($th->getCode()==401){
+                    $serverToken->status = 'inactive';
+                    $serverToken->save();
+                    $success = false;
+                };
+            }
+
+        } while ($success == false);
+
 
         echo $response->getBody();
     }
